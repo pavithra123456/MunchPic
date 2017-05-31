7c7778494baa3398ac863362a9928e822cf5165a
Pavithra Jayanna <PJayanna@opteamix.com>
2017-05-31 10:07:23 +0530

    storyboard changes

--- Munchpic/Controller/HomeTabViewController.swift
+++ Munchpic/Controller/HomeTabViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class HomeTabViewController: UIViewController {
+class HomeTabViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
