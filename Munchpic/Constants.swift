c98daf954481fbf35aef0f9d33cbe60be9d345cf
Pavithra Jayanna <PJayanna@opteamix.com>
2017-05-31 21:48:02 +0530

    added API names to constants

--- Munchpic/Constants.swift
+++ Munchpic/Constants.swift
@@ -10,5 +10,37 @@ import Foundation
 class Constants{
     
     static let kBaseUrl : String = "http://ekalavyatech.com/munchpic.com/munchpicPHP/"
-    static let kGetPost : String = "GETPosts"
+    
+    static let kGetPost : String = "GetPosts.php"
+    static let kGetCollections : String = "getCollections.php"
+    static let kGetIngredients : String = "getIngredients.php"
+    static let kGetComments : String = "getComments.php"
+    static let kGetCornivals : String = "getCornivals.php"
+    static let kGetCollectionCategories : String = "getCollectionCategories.php"
+    static let getLoves : String = "getLoves.php"
+    static let getParticularPost : String = "getParticularPost.php"
+    static let getTrends : String = "getTrends.php"
+    static let getUserPosts : String = "getUserPosts.php"
+
+    static let kInsertLoves : String = "insertLoves.php"
+    static let insertCollections : String = "insertCollections.php"
+    static let insertComments : String = "insertComments.php"
+    static let insertPost : String = "insertPost.php"
+    
+    static let kDeleteCollection : String = "deleteCollection.php"
+
+    static let fileUpload : String = "fileUpload.php"
+    static let userFileUpload : String = "userFileUpload.php"
+
+    static let forgotPassword : String = "forgotPassword.php"
+    static let login : String = "login.php"
+    static let resetPassword : String = "resetPassword.php"
+    static let updateUserInfo : String = "updateUserInfo.php"
+    static let updateUserPost : String = "updateUserPost.php"
+    
+    static let getUserInfo : String = "getUserInfo.php"
+    static let register : String = "register.php"
+    static let activation : String = "activation.php"
+    static let imageUpload : String = "imageUpload.php"
+    static let videoUpload : String = "videoUpload.php"
 }
