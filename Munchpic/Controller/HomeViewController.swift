42606e8c4ce7bbaf87bdf589f4288959f4626bba
chaitanya <chaitanyakumari4b5@gmail.com>
2017-06-06 18:25:38 +0530

    Home and window design added

--- Munchpic/Controller/HomeViewController.swift
+++ Munchpic/Controller/HomeViewController.swift
@@ -8,28 +8,122 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
-
+class HomeViewController: UIViewController ,UITableViewDelegate,UITableViewDataSource {
+    
+    @IBOutlet var Pagingimage: UIImageView!
+    @IBOutlet var pagecontroller: UIPageControl!
+    @IBOutlet var mscrollview: UIScrollView!
+    @IBOutlet var Hometableview: UITableView!
+    
+    
+    var slidearrayData:NSMutableArray = NSMutableArray()
+    var mPosition:Int = Int()
+    var myint = Int()
+    var index = 0
+    let swipeGestureLeft = UISwipeGestureRecognizer()
+    let swipeGestureRight = UISwipeGestureRecognizer()
+    var slidetimer: Timer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        slidearrayData = ["1.png","2.png","3.png","1.png","2.png","3.png","1.png","2.png"]
+        
+        self.swipeGestureLeft.direction = UISwipeGestureRecognizerDirection.left
+        self.swipeGestureRight.direction = UISwipeGestureRecognizerDirection.right
+        self.swipeGestureLeft.addTarget(self, action:#selector(HomeViewController.handleSwipeLeft(gesture:)))
+        self.swipeGestureRight.addTarget(self, action:#selector(HomeViewController.handleSwipeRight(gesture:)))
+        mscrollview.addGestureRecognizer(self.swipeGestureLeft)
+        mscrollview.addGestureRecognizer(self.swipeGestureRight)
+        
+        slidetimer = Timer.scheduledTimer(timeInterval: 5, target: self, selector:#selector(HomeViewController.handleSwipeLeft(gesture:)), userInfo: nil, repeats: true)
+        
     }
-
+    
+    @IBAction func Pagecontroller_Action(_ sender: Any) {
+        
+        let servicesList1: (AnyObject) = (slidearrayData[(pagecontroller.currentPage)] as AnyObject)
+        Pagingimage.image = UIImage(named: servicesList1 as! String)
+        mPosition = pagecontroller.currentPage
+        
+    }
+    
+    //Left gesture
+    func handleSwipeLeft(gesture: UISwipeGestureRecognizer){
+        
+        if pagecontroller.currentPage < slidearrayData.count-1 {
+            pagecontroller.currentPage += 1
+            let servicesList1: (AnyObject) = (slidearrayData[(pagecontroller.currentPage)] as AnyObject)
+            Pagingimage.image = UIImage(named: servicesList1 as! String)
+            mPosition = pagecontroller.currentPage
+        }else{
+            pagecontroller.currentPage = 0
+            let servicesList1: (AnyObject) = (slidearrayData[(pagecontroller.currentPage)] as AnyObject)
+            Pagingimage.image = UIImage(named: servicesList1 as! String)
+            mPosition = pagecontroller.currentPage
+            
+        }
+    }
+    
+    // Right gesture
+    func handleSwipeRight(gesture: UISwipeGestureRecognizer){
+        
+        if pagecontroller.currentPage != 0 {
+            pagecontroller.currentPage -= 1
+            let servicesList1: (AnyObject) = (slidearrayData[(pagecontroller.currentPage)] as AnyObject)
+            Pagingimage.image = UIImage(named: servicesList1 as! String)
+            mPosition = pagecontroller.currentPage
+            
+        }else{
+            
+            pagecontroller.currentPage = slidearrayData.count
+            let servicesList1: (AnyObject) = (slidearrayData[(pagecontroller.currentPage)] as AnyObject)
+            Pagingimage.image = UIImage(named: servicesList1 as! String)
+            mPosition = pagecontroller.currentPage
+            
+        }
+        
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return slidearrayData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var cell : CustomTableViewCell! = Hometableview.dequeueReusableCell(withIdentifier: "Hcell") as! CustomTableViewCell
+        if(cell == nil)
+        {
+            cell = Bundle.main.loadNibNamed("Hcell", owner: self, options: nil)?[0] as! CustomTableViewCell;
+        }
+        
+        let servicesList1: (AnyObject) = (slidearrayData[indexPath.row] as AnyObject)
+        cell.foodrecipeImage.image = UIImage(named: servicesList1 as! String)
+        
+        
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
+
+
--- Munchpic/Controller/WindowViewController.swift
+++ Munchpic/Controller/WindowViewController.swift
@@ -33,3 +33,44 @@ class WindowViewController: UIViewController {
     */
 
 }
+
+extension WindowViewController : UITableViewDelegate { }
+
+extension WindowViewController : UITableViewDataSource {
+    
+    //    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    //        return categories[section]
+    //    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! CustomTableViewCell
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        
+        let headerView:UIView =  UIView()
+        let imageViewGame = UIImageView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 100));
+        let image = UIImage(named: "3.png");
+        imageViewGame.image = image;
+        headerView.addSubview(imageViewGame)
+        
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        
+        return 100
+    }
+    
+    
+    
+}
