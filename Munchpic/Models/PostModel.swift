99753c12fdf39574837838e3271ae8662d0a6144
Pavithra Jayanna <PJayanna@opteamix.com>
2017-06-24 16:59:25 +0530

    Added images

---
+++ Munchpic/Models/PostModel.swift
@@ -0,0 +1,13 @@
+//
+//  PostModel.swift
+//  Munchpic
+//
+//  Created by Pavithra Jayanna on 24/06/17.
+//  Copyright © 2017 Pavithra Jayanna. All rights reserved.
+//
+
+import UIKit
+
+class PostModel: NSObject {
+
+}
---
+++ Munchpic/Models/TrendsModel.swift
@@ -0,0 +1,13 @@
+//
+//  TrendsModel.swift
+//  Munchpic
+//
+//  Created by Pavithra Jayanna on 24/06/17.
+//  Copyright © 2017 Pavithra Jayanna. All rights reserved.
+//
+
+import UIKit
+
+class TrendsModel: NSObject {
+
+}
